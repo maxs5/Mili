61fad338b23a2c1d0f3a6e7584e9233eb067fe7e
maxs5 <93215847+maxs5@users.noreply.github.com>
2021-12-30 14:20:08 +0300

    init

---
+++ src/Mili.java
@@ -0,0 +1,25 @@
+public class Mili {
+    public static void Mili(String[]args){
+        int a = 10;
+        int b = 30;
+        int sum = a + b;
+        System.out.println(sum);// 40
+    }
+}
+
+
+
+
+
+public class Mili {
+    public static void Mili(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int x = (a + b) / (a * b);
+        double y = (double) (a + b) / (a * b);
+
+        System.out.println("int x: " + x);
+        System.out.println("double y: " + y);
+    }
+}
